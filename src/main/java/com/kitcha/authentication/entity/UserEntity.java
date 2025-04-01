17ea30032eeb5e94df302f2b268eb9f458d6a2bf
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-04-02 02:35:22 +0900

    Update UserEntity.java

--- src/main/java/com/kitcha/authentication/entity/UserEntity.java
+++ src/main/java/com/kitcha/authentication/entity/UserEntity.java
@@ -7,23 +7,21 @@ import lombok.Data;
 @Table(name = "user")
 @Data
 public class UserEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(name = "nickname", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+    @Column(nullable = false)
     private String nickname;
 
-    @Column(name = "email", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+    @Column(nullable = false)
     private String email;
 
-    @Column(name = "password", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+    @Column(nullable = false)
     private String password;
 
-    @Column(name = "role", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
+    @Column(nullable = false)
     private String role;
 
-    @Column(name = "interest", columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci")
     private String interest;
 }
